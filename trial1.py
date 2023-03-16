733f79875b51392d311e8b565f4e446e74fa536d
Patience <115657310+MsMusenya@users.noreply.github.com>
2023-03-16 15:07:53 +0300

    Add files via upload
    ### **Learn about Kenya from a Kenya**
    
    - They say the best way to learn is practically, learning in reverse. 
    - Here we don't start with the nuggets but rather the questions
    - Don't worry, if you get it wrong you'll still get to learn since the solution will be given
    - In Kenya, we have the concept of "Nyumba Kumi" which directly translated to 10 houses. It is an initiative which was started of in Kenya to encourage the locals a to constantly interact and share information about one another.
    - It was put into effect in 2013 as a measure against crime and terrorism in communities.
    - However, we will borrow the spirit it embodies(sharing information about oneself) in order to overcome the myths the international community has in regards to Kenya.
    - Here, you get to learn from a native who is born, raised and bred in Kenya(though curious about Europe and would love to experience the places like the natives do)
    - Whatever you know, some are true and while others are just far fetched.
    - This project aims to shed a white light over all the information you have while adding a few more that you may not know but definitely should.
    - Buckle up, this is going to be one fun ride. I had so much fun coming up with this and I hope you have just as much learning

---
+++ trial1.py
@@ -0,0 +1,41 @@
+print("All answers should be in Uppercase! Get to know Kenya and some random fun facts here and there. Let's go!")
+while True:
+    answer1 = input("What continent is Kenya in? : \n A) Africa.\n B) Asia.\n C) Europe.\n D)Antarctica.\n [A/B/C/D]? : ")
+    if answer1 == "A" :
+        print("Congrats! You got the answer!")
+        print()
+        break
+    else:
+        print("Incorrect Answer! The correct answer is: Africa")
+        print()
+        break
+while True:
+    answer2 = input("How many counties does Kenya have?: \n A) 49.\n B) 47. \n C) 40.\n D) A lot.\n [A/B/C/D]? : ")
+    if answer2 == "B":
+        print("Nice job! You got it right")
+        print()
+        break
+    else:
+        print("Unfortunately, that's not it. There are 47 counties in Kenya.")
+        print()
+        break
+while True:
+    answer3 = input("What activity opened Kenya up to the international commmunity : \n A) Trade\N B) Religion \n C) Exploration\n D)European expansion \n [A/B/C/D]? : ")
+    if answer3 == "A":
+        print("Congrats! Here's a fun fact!\n Trade activities was the earliest dated activity on the Kenyan coast that opened the country up to the international community")
+        print()
+        break
+    else:
+        print("Incorrect Answer! The correct answer is: A. Trade")
+        print()
+        break
+while True:
+    answer4 = input("How many tribes are there in Kenya: \n A) 45 \nB) 47 \n C) 49 \n D) Many \n [A/B/C/D]? : ")
+    if answer4 == "A":
+        print("That's right! You are really good at this, maybe get it a little more interesting. Buckle up")
+        print()
+        break
+    else:
+        print("Incorrect Answer! The correct answer is: A. 45 . \n Fun fact, Kenya had provinces before the revision on administrative duties following the promulgation of the 2010 Kenyan Constitution")
+        print()
+        break
